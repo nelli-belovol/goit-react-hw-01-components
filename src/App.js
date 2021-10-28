343e0dab5d5c97723ad6bae7f93a828eefb36b67
nelli-belovol <nelli199020@gmail.com>
2021-10-28 21:53:39 +0300

    task-1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from "./logo.svg";
-import "./App.css";
+import './App.css';
+import { Profile } from './components/Profile/Profile.js';
+import user from './components/Profile/Profile.json';
 
+console.log(user);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </>
   );
 }
 
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types';
+import s from './Profile.module.scss';
+console.log(s);
+
+export function Profile({ name, tag, location, avatar, stats }) {
+  return (
+    <>
+      <div className={s.profile}>
+        <div className={s.description}>
+          <img src={avatar} alt={name} className={s.avatar} />
+          <p className={s.name}>{name}</p>
+          <p className={s.text}>@{tag}</p>
+          <p className={s.text}>{location}</p>
+        </div>
+
+        <ul className={s.stats}>
+          <li className={s.stats__item}>
+            <span className={s.text}>Followers</span>
+            <span className={s.quantity}>{stats.followers}</span>
+          </li>
+          <li className={s.stats__item}>
+            <span className={s.text}>Views</span>
+            <span className={s.quantity}>{stats.views}</span>
+          </li>
+          <li className={s.stats__item}>
+            <span className={s.text}>Likes</span>
+            <span className={s.quantity}>{stats.likes}</span>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+Profile.defaultProps = {
+  name: 'User',
+  avatar: '../../logo.svg',
+};
+
+Profile.propTypes = {
+  name: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.objectOf(PropTypes.number),
+};
